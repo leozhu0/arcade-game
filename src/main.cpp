74f2f75ffed6534562c69ea2cd82538ea562c78b
Kevin Lee <kevinlee@169-231-117-10.wireless.ucsb.edu>
2023-11-28 18:21:08 -0800

    removed junk files

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,62 @@
+#include "lib/player.h"
+#include "lib/mobs.h"
 #include <GL/glut.h>
 
+Player player(0.0f, -0.5f); // Instantiate a player object at the initial position
+Mobs mobs; // Instantiate a Mobs object
+
 void init() {
-  glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+void update(int value) {
+    player.update(); // Update the player object
+    mobs.update();   // Update the mobs object
+    glutPostRedisplay(); // Request a redraw
+    glutTimerFunc(16, update, 0); // Schedule the next update after 16 milliseconds
 }
 
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT);
-  // Drawing code goes here
-  // For example, you can draw a triangle, a square, etc.
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  glFlush(); // Ensures that the drawing commands are actually executed
+    // Draw background
+    glLoadIdentity();
+    glBegin(GL_QUADS);
+    glColor3f(0.0, 0.0, 0.0); // Set the background color to black
+    glVertex2f(-1.0, 1.0);    // Top-left
+    glVertex2f(1.0, 1.0);     // Top-right
+    glVertex2f(1.0, -1.0);    // Bottom-right
+    glVertex2f(-1.0, -1.0);   // Bottom-left
+    glEnd();
+
+    // Draw the player
+    player.draw();
+
+    // Draw the mobs
+    mobs.draw();
+
+    glutSwapBuffers(); // Use double buffering
+}
+
+void timer(int value) {
+    update(value);
+    glutPostRedisplay(); // Request a redraw
+    glutTimerFunc(16, timer, value); // Schedule the next update after 16 milliseconds
+}
+
+void handleMouseMotion(int newX, int newY) {
+    // Update player position based on mouse coordinates
+    player.handleMouseMotion(newX, newY);
 }
 
 int main(int argc, char** argv) {
-  glutInit(&argc, argv); // Initialize GLUT
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Set the display mode
-  glutInitWindowSize(800, 600); // Set the window size
-  glutInitWindowPosition(100, 100); // Set the window position
-  glutCreateWindow("OpenGL GLUT Window"); // Create the window with a title
-
-  init(); // Call the initialization function
-
-  glutDisplayFunc(display); // Tell GLUT to use the display function defined above
-
-  glutMainLoop(); // Start GLUT's main loop
-  return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutCreateWindow("Bullet Hell");
+    glutFullScreen(); // Uncomment this line if you want the window to be fullscreen
+    glutDisplayFunc(display);
+    glutPassiveMotionFunc(handleMouseMotion);
+    glutTimerFunc(25, timer, 0);
+    glutMainLoop();
+    return 0;
 }
