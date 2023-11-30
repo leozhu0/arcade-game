52e5f64167f532931f31f644c5487bb28a77dcf5
Kevin Lee <kevinlee@169-231-117-10.wireless.ucsb.edu>
2023-11-30 10:57:58 -0800

    small changes to format

--- src/lib/player.cpp
+++ src/lib/player.cpp
@@ -8,20 +8,24 @@ Player::Player(float initialX, float initialY) : x(initialX), y(initialY) {
     int width, height, channels;
     
     // Load image and check for errors
-    unsigned char* image = stbi_load("src/lib/images/testCannon.png", &width, &height, &channels, STBI_rgb);
+    unsigned char* image = stbi_load("lib/images/testCannon.png", &width, &height, &channels, STBI_rgb);
     if (!image) {
         std::cerr << "Error loading image: " << stbi_failure_reason() << "\n";
         return;  // Return or handle the error appropriately
     }
     else {
-        std::cout << "Image loaded successfully. Width: " << width << ", Height: " << height << ", Channels: " << channels << "\n";
+        std::cout << "Image loaded successfully. Width: " << width << ", Height: " << height << ", Channels: " << channels << "\n"; //FIXME
     }
 
     // Create OpenGL texture
+    // glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
+
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // GL_NEAREST = no smoothing
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); // GL_NEAREST = no smoothing
 
     // Free image data
     stbi_image_free(image);
@@ -37,9 +41,10 @@ void Player::draw() const {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, textureID);
+    // glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 
     glBegin(GL_QUADS);
-    glColor4f(1.0, 1.0, 1.0, 1.0);  // Specify alpha (transparency)
+    glColor4f(1.0, 1.0, 1.0, 0.5);  // Specify alpha (transparency)
 
     // Specify texture coordinates for each vertex (in the range [0, 1])
     glTexCoord2f(0.0, 0.0); glVertex2f(x - 0.1, y + 0.1);
